f2b702ced80d7dad015560cad1620af545c09b62
sindhuja92 <sivutukuri@mix.wvu.edu>
2019-04-23 23:52:31 -0400

    Update bump-timestamp-file.sh

--- tutorials/basic/publishing-outputs/bump-timestamp-file.sh
+++ tutorials/basic/publishing-outputs/bump-timestamp-file.sh
@@ -8,8 +8,8 @@ git clone resource-gist updated-gist
 cd updated-gist
 echo $(date) > bumpme
 
-git config --global user.email "nobody@concourse-ci.org"
-git config --global user.name "Concourse"
+git config --global user.email "sivutukuri@mix.wvu.edu"
+git config --global user.name "sindhuja92"
 
 git add .
 git commit -m "Bumped date"
